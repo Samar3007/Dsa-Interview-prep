87c6c8cd02c0c4abefa694f166732480cf601ab2
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2023-04-18 12:51:31 +0530

    Time: 27 ms (74.75%), Space: 22.4 MB (62.81%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -6,21 +6,13 @@ public:
         int mid=0;
         while(low<=high){
             mid=(low+high)/2;
-            if (mid % 2 == 0) {
-                if (nums[mid] == nums[mid + 1]) 
+            if (nums[mid] == nums[mid^1]) 
                 //Checking whether we are in right half
-                    low = mid + 1;  
+                low = mid + 1;  
                      //Shrinking the right half
-                else
-                    high = mid - 1; //Shrinking the left half
-            } else {
-
-                //Checking whether we are in right half
-                if (nums[mid] == nums[mid + 1]) 
-                    high = mid - 1; //Shrinking the right half                    
-                else
-                    low = mid + 1; //Shrinking the left half
-            }
+            else
+                high = mid - 1; //Shrinking the left half
+            
         }
         return nums[low];
     }
