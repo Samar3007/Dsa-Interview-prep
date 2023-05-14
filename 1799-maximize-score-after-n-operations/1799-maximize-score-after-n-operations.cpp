2ecf14b90f357b90ab51fe9ddbcdd2c9961be56b
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2023-05-14 15:10:34 +0530

    Time: 546 ms (14.88%), Space: 16.1 MB (29.07%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
-    map<int,int>m;
-    int c(int t,int mask,vector<int>&nums){
-        if(t==0)return 0;
-        if(m[mask])return m[mask];
-        int ans=0;
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<nums.size();j++){
-                if((mask & (1<<i)) && (mask & (1<<j)) && (i!=j)){
-                    ans=max(ans,t*__gcd(nums[i],nums[j]) + c(t-1,mask^(1<<i)^(1<<j),nums));
-                }
+    int gcd(int num1,int num2) {
+        
+        if(num2 == 0) 
+            return num1;
+        
+        return gcd(num2,num1 % num2);
+
+    }
+    int helper(vector<int>& nums,int mask,int op,unordered_map<int,int> &mp) {
+        if(mp.find(mask) != mp.end()) // checking if the combination is already computed or not ,if it is return it
+            return mp[mask];
+
+        mp[mask] = 0; // intialising the key with 0 
+
+        // checking every possible pairs
+        for(int i = 0;i < nums.size();i++) {
+            for(int j = i + 1;j < nums.size();j++) {
+                if(((1 << i) & mask )|| (1 << j) & mask) // checking the num at ith and jth position is not used if it is we skip the iteration
+                    continue;
+                
+                int newMask = mask | (1 << i) | (1 << j); // now marking the ith and jth 1 which indicated that it's used
+
+                int score = op * gcd(nums[i],nums[j]); // computing the score
+
+                mp[mask] = max(mp[mask],score + helper(nums,newMask,op + 1,mp)); // making recursive call for the next set of combinations
+
             }
         }
-        m[mask]=ans;
-        return ans;
+
+        return mp[mask]; // return the score
     }
     int maxScore(vector<int>& nums) {
-        int n=nums.size();
-        int mask=(1<<n)-1;
-        return c(n/2,mask,nums);
+        unordered_map<int,int> mp;
+        return helper(nums,0,1,mp);
     }
 };
