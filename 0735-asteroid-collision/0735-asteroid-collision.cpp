5ed6c0a3cf1c708a664cc86238afa551fecee089
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2023-06-17 03:27:16 +0530

    Time: 10 ms (92.94%), Space: 17.3 MB (99.23%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,33 +1,15 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& ast) {
-        stack<int> s;
-        for(int i=0;i<ast.size();i++){
-            
-            if(s.empty() || ast[i]>0) s.push(ast[i]);
-            
-            else{
-                while(!s.empty() && s.top()>0 && abs(ast[i])>s.top()){
-                    s.pop();
-                }
-                if(!s.empty() && s.top() == abs(ast[i])) {
-                    s.pop();
-                }
-                else {
-                    if(s.empty() || s.top() < 0) {
-                        s.push(ast[i]);
-                    }
-                }
-            }
-        
+    vector<int> asteroidCollision(vector<int>& a) {
+        vector<int> s; // use vector to simulate stack.
+        for (int i = 0; i < a.size(); i++) {
+            if (a[i] > 0 || s.empty() || s.back() < 0) // a[i] is positive star or a[i] is negative star and there is no positive on stack
+                s.push_back(a[i]);
+            else if (s.back() <= -a[i]) { // a[i] is negative star and stack top is positive star
+                if(s.back() < -a[i]) i--; // only positive star on stack top get destroyed, stay on i to check more on stack.
+                s.pop_back(); // destroy positive star on the frontier;
+            } // else : positive on stack bigger, negative star destroyed.
         }
-        
-        vector<int> ans(s.size());
-        for(int i=ans.size()-1;i>=0;i--){
-            ans[i]=s.top();
-            s.pop();
-        }
-        return ans;
+        return s;
     }
 };
-
