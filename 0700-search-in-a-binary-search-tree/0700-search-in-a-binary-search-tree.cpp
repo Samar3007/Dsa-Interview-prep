e853d034fc69f7ebbd66a42559c83c23c758edf3
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2023-06-20 15:21:44 +0530

    Time: 42 ms (74.60%), Space: 34.7 MB (41.78%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
@@ -11,23 +11,24 @@
  */
 class Solution {
 public:
-    TreeNode *node=new TreeNode();
-    TreeNode* searchBST(TreeNode* root, int val) {
-        //if root is null return null
-        if(!root) return root;
-         
-        // if root->val != val,search in left and right
-        //otherwise this would be required node and we would return it
-        if(val<root->val){
-            //search in left 
-            node=searchBST(root->left,val);
-        } else if(val>root->val){
-            //search in right
-            node=searchBST(root->right,val);
-        } else {
-            //required node
-            node=root;
+    TreeNode* ans=NULL;
+    TreeNode* searchBST(TreeNode* root, int val){
+        if(root->val==val) ans=root; 
+     
+        if(root->left){
+            if(ans!=NULL) return ans;
+            searchBST(root->left,val);
+            
         }
-        return node;
+        
+        if(root->right){
+            if(ans!=NULL) return ans;
+            searchBST(root->right,val);
+            
+        }
+        
+        if(ans!=NULL) return ans;
+        return NULL;
     }
+    
 };
