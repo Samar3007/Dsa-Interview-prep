3336b527a33984b17f750df383954e9abc8d1b68
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2023-07-05 03:29:12 +0530

    Time: 3 ms (34.89%), Space: 5.9 MB (73.50%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -4,12 +4,12 @@ public:
     int climbStairs(int n) {
         
         int prev2=1, prev1=1;
-        int curr=1;
+        
         for(int i=2;i<=n;i++){
-            curr=prev1+prev2;
+            int curr=prev1+prev2;
             prev2=prev1;
             prev1=curr;
         }
-        return curr;
+        return prev1;
     }
 };
