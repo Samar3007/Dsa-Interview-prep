afbeb8b2c8fc2e8b20eb871fcc9d15e6e8e28c41
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2023-07-05 02:58:18 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (97.91%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    
-
-    int func(int row, int col, vector< vector< int> > &mat, vector< vector< int> > &dp){
-        if(row>=0 && col>=0 && mat[row][col]==1) return 0;
-        if(row==0 && col==0) return 1;
-        if(row<0 || col<0 ) return 0;
-        if(dp[row][col]!=-1) return dp[row][col];
-        int l=func(row-1,col,mat,dp);
-        int r=func(row,col-1,mat,dp);
-        return dp[row][col]=(l+r);
-
-    }
-
     int uniquePathsWithObstacles(vector<vector<int>> &mat) {
         int n=mat.size(), m=mat[0].size(); 
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return func(n-1,m-1,mat,dp);
+        int dp[n][m];
+        
+        for(int row=0;row<n;row++){
+            for(int col=0;col<m;col++){
+                if(mat[row][col]==1) dp[row][col]=0;
+                else if(row==0 && col==0) dp[row][col]=1;
+                else{
+                    int l=0,r=0;
+                    if(row>0) l=dp[row-1][col];
+                    if(col>0) r=dp[row][col-1];
+                    dp[row][col]=(l+r);
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
