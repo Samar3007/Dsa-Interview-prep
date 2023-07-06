1be1aeff93ae33300939b9a3d097578fdf8750dd
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2023-07-06 16:56:09 +0530

    Time: 12 ms (55.61%), Space: 10.3 MB (14.85%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -4,23 +4,27 @@ public:
     int minPathSum(vector<vector<int> > &matrix){
         int n=matrix.size(), m=matrix[0].size();
         vector<vector<int> > dp(n,vector<int>(m,-1));
+        vector<int> prev(m,0);
         
         for(int i=0;i<n;i++){
+            
+            vector<int> curr(m,0);
             for(int j=0;j<m;j++){
                 
-                if(i==0 && j==0) dp[0][0]=matrix[0][0];
+                if(i==0 && j==0) curr[0]=matrix[0][0];
                 
                 else{
                     int left=1e9,up=1e9;
-                    if(i>0) up = matrix[i][j] + dp[i-1][j];
-                    if(j>0) left = matrix[i][j] + dp[i][j-1];
-                    dp[i][j]=min(left,up);
+                    if(i>0) up = matrix[i][j] + prev[j];
+                    if(j>0) left = matrix[i][j] + curr[j-1];
+                    curr[j]=min(left,up);
                 }
                 
             }
+            prev=curr;
         }
 
-        return dp[n-1][m-1];
+        return prev[m-1];
 
     }
 };
