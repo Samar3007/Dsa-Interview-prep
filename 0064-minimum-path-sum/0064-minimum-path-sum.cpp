93c004abbf3a5400feba38fb0c5c8b6e0a87c50a
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2023-07-06 16:50:16 +0530

    Time: 12 ms (55.61%), Space: 10.2 MB (14.85%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    
-    int func(int i, int j,vector<vector<int>> &matrix,vector<vector<int>> &dp){
-        if(i==0 && j == 0) return matrix[0][0];
-
-        if(i<0 || j<0) return 1e9;
-
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        int up = matrix[i][j] + func(i-1,j,matrix,dp);
-        int left = matrix[i][j] + func(i,j-1,matrix,dp);
-
-        return dp[i][j] = min(up,left);
-
-    }
 
     int minPathSum(vector<vector<int> > &matrix){
         int n=matrix.size(), m=matrix[0].size();
         vector<vector<int> > dp(n,vector<int>(m,-1));
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                
+                if(i==0 && j==0) dp[0][0]=matrix[0][0];
+                
+                else{
+                    int left=1e9,up=1e9;
+                    if(i>0) up = matrix[i][j] + dp[i-1][j];
+                    if(j>0) left = matrix[i][j] + dp[i][j-1];
+                    dp[i][j]=min(left,up);
+                }
+                
+            }
+        }
 
-        return func(n-1,m-1,matrix,dp);
+        return dp[n-1][m-1];
 
     }
 };
