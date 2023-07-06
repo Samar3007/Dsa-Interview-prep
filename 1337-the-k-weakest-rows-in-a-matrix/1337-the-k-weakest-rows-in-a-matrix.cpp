0d950d2d5e066ab9240e070e8df49e07232bb541
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2023-07-07 04:46:42 +0530

    Time: 12 ms (75.87%), Space: 10.8 MB (9.25%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
+     int binary(vector<int> m)
+    {
+        int l = 0;
+        int h = m.size()-1;
+        while(l <= h)
+        {
+            int mid = l + (h-l)/2;
+            if(m[mid] == 0)
+                h = mid - 1;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+    
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> cnt;
         for(int i=0;i<mat.size();i++){
-            int ones=0;
-            for(int j=0;j<mat[i].size();j++){
-                if(mat[i][j]==1) ones++;
-            }
+            int ones=binary(mat[i]);
             cnt.push({ones,i});
         }
         vector<int> ans;
