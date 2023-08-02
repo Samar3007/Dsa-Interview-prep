79a91018c6944ead0c614e3468973868ac631fa2
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2023-08-02 19:49:09 +0530

    Time: 0 ms (100.00%), Space: 8 MB (20.03%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void solve(vector<int>&a,int i){
+        if(i==a.size()){
+            ans.push_back(a);
+            return;
+        }
+        for(int j=i;j<a.size();j++){
+            swap(a[j],a[i]);  
+            solve(a,i+1);
+            swap(a[j],a[i]); // backtrack
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        solve(nums,0);
+        return ans;
+        
+        
+    }
+};
