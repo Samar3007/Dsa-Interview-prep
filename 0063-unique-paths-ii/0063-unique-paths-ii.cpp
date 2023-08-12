fddb638f609f74c221365814e55822089579f54d
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2023-08-12 14:50:48 +0530

    Time: 5 ms (28.03%), Space: 7.6 MB (81.25%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -4,18 +4,25 @@ public:
         int n=mat.size(), m=mat[0].size(); 
         int dp[n][m];
         
+        vector<int> prev(m,0);
+        
         for(int row=0;row<n;row++){
+            vector<int> temp(m,0);
+            
             for(int col=0;col<m;col++){
-                if(mat[row][col]==1) dp[row][col]=0;
-                else if(row==0 && col==0) dp[row][col]=1;
+            
+                if(mat[row][col]==1) temp[col]=0;
+                else if(row==0 && col==0) temp[col]=1;
                 else{
                     int l=0,r=0;
-                    if(row>0) l=dp[row-1][col];
-                    if(col>0) r=dp[row][col-1];
-                    dp[row][col]=(l+r);
+                    if(row>0) l=prev[col];
+                    if(col>0) r=temp[col-1];
+                    temp[col]=(l+r);
                 }
             }
+            prev=temp;
+            
         }
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
 };
