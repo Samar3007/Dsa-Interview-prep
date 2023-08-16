6c8045b94736ca4516d85b302e14ad94f556f2c8
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2023-08-16 18:08:49 +0530

    Time: 183 ms (98.56%), Space: 134.7 MB (66.10%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -4,9 +4,11 @@ public:
         deque<int> q;
         vector<int> ans;
         for(int i=0;i<nums.size();i++){
-            if(!q.empty() && q.front()==i-k) q.pop_front();
             while(!q.empty() && nums[i]>=nums[q.back()]){
                 q.pop_back();
+            }
+            if(!q.empty() && q.front()<=i-k){
+                q.pop_front();
             }
             q.push_back(i);
             if(i>=k-1) ans.push_back(nums[q.front()]);
