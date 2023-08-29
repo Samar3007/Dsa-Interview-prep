f4864d3e968c396234b08d0fb2fb7f5b9a32ae62
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2023-08-30 00:45:20 +0530

    Added solution - LeetHub

---
+++ City With the Smallest Number of Neighbors at a Threshold Distance - GFG/city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int findCity(int n, int m, vector<vector<int>>& edges, int distanceThreshold) {
+        
+        vector<vector<int>> mat(n,vector<int>(n,1e9));
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            int w=edges[i][2];
+            mat[u][v]=w;
+            mat[v][u]=w;
+        }
+        
+        for(int i=0;i<n;i++){
+            mat[i][i]=0;
+        }
+        
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    mat[i][j]=min(mat[i][j],mat[i][k]+mat[k][j]);      
+                }
+            }
+        }
+        
+        int ans=0;
+        int mini=INT_MAX;
+        for(int i=0;i<n;i++){
+            int cnt=0;
+            for(int j=0;j<n;j++){
+                if(mat[i][j]<=distanceThreshold && i!=j){
+                    cnt++;
+                }
+            }
+            if(mini>=cnt){
+                mini=cnt;
+                ans=i;
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> adj;
+        // n--;
+        for (int i = 0; i < m; ++i) {
+            vector<int> temp;
+            for (int j = 0; j < 3; ++j) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
+
+        int dist;
+        cin >> dist;
+        Solution obj;
+        cout << obj.findCity(n, m, adj, dist) << "\n";
+    }
+}
+
+// } Driver Code Ends
