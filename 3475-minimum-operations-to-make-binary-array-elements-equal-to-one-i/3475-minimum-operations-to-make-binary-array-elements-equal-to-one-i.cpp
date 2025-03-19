f31f0960ce9eabec46242a11cf18f40ccc4a4e6b
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2025-03-19 12:29:04 +0530

    Time: 14 ms (19.6%), Space: 140 MB (47.55%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -5,16 +5,13 @@ public:
         for(int i=0;i<n-2;i++){
             if(nums[i]==0){
                 nums[i]=1;
-                if(nums[i+1]) nums[i+1]=0;
-                else nums[i+1]=1;
-                if(nums[i+2]) nums[i+2]=0;
-                else nums[i+2]=1;
+                nums[i] ^= 1;
+                nums[i + 1] ^= 1;
+                nums[i + 2] ^= 1;
                 cnt++;
             }
         }
-        for(int i=0;i<n;i++){
-            if(nums[i]==0) return -1;
-        }
+        if(nums[n-2]==0 || nums[n-1]==0) return -1;
         return cnt;
     }
 };
