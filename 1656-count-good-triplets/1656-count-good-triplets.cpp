5a558cbd41d4eabb6248f4e52c88f10adbae21b6
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2025-04-14 10:17:17 +0530

    Time: 9 ms (41.98%), Space: 11.1 MB (43.88%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.cpp
+++ 1656-count-good-triplets/1656-count-good-triplets.cpp
@@ -5,9 +5,11 @@ public:
         int cnt=0, n=arr.size();
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
-                    if(abs(arr[i] - arr[j])<=a && abs(arr[j] - arr[k])<=b && abs(arr[i] - arr[k])<=c){
-                        cnt++;
+                if(abs(arr[i]-arr[j])<=a){
+                    for(int k=j+1;k<n;k++){
+                        if(abs(arr[j] - arr[k])<=b && abs(arr[i] - arr[k])<=c){
+                            cnt++;
+                        }
                     }
                 }
             }
