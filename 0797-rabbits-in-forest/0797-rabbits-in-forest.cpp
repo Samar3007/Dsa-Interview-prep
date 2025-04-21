f29327115bfb2337eee5d7bf281379bc74f0f204
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2025-04-21 14:12:58 +0530

    Time: 0 ms (100%), Space: 12.3 MB (10.6%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
@@ -2,14 +2,9 @@ class Solution {
 public:
     int numRabbits(vector<int>& answers) {
         int ans=0;
-        unordered_map<int,int> mpp;
+        int arr[1000]={0};
         for(int it:answers){
-            if(it==0) ans++;
-            else{
-                if(mpp[it]==0) ans+=it+1;
-                mpp[it]++;
-                if(mpp[it]==it+1) mpp[it]=0;
-            }
+            if(arr[it]++%(it+1) == 0) ans+=it+1;
         }
         return ans;
     }
