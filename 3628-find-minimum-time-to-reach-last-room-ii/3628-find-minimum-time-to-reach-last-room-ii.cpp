7418204bf55969b2ca91e55b3dcbf771dce5d61a
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2025-05-08 13:45:41 +0530

    Time: 772 ms (32.7%), Space: 221.2 MB (17.3%) - LeetHub

--- 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.cpp
+++ 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    typedef pair<int,pair<int,pair<int,bool>>> P;
+    typedef pair<int,pair<int,int>> P;
     vector<vector<int>> dir{{0,1},{0,-1},{-1,0},{1,0}};
     
     int minTimeToReach(vector<vector<int>>& mT) {
@@ -10,13 +10,12 @@ public:
         res[0][0]=0;
         
         priority_queue<P,vector<P>,greater<P>> pq;
-        pq.push({0,{0,{0,false}}});
+        pq.push({0,{0,0}});
         
         while(!pq.empty()){
             int curr_time=pq.top().first;
             int i=pq.top().second.first;
-            int j=pq.top().second.second.first;
-            bool alt=pq.top().second.second.second;
+            int j=pq.top().second.second;
             pq.pop();
             if(i==n-1 && j==m-1){
                 return curr_time;
@@ -25,12 +24,11 @@ public:
                 int i_=i+it[0];
                 int j_=j+it[1];
                 if(i_>=0 && i_<n && j_>=0 && j_<m){
-                    int arrTime=max(mT[i_][j_],curr_time);
-                    if(alt) arrTime+=2;
-                    else arrTime+=1;
+                    int move=(i_+j_)%2==0 ? 2 : 1;
+                    int arrTime=max(mT[i_][j_],curr_time)+move;
                     if(res[i_][j_]>arrTime){
                         res[i_][j_]=arrTime;
-                        pq.push({arrTime,{i_,{j_,!alt}}});
+                        pq.push({arrTime,{i_,j_}});
                     }
                 }
             }
