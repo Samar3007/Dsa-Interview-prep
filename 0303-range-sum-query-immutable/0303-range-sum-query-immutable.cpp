b4b9aac56b4d0b94072cdd30bd7f14afa3ea0f79
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2025-08-06 15:37:21 +0530

    Time: 82 ms (16.13%), Space: 24.3 MB (22.97%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -1,16 +1,39 @@
 class NumArray {
 public:
-    vector<int> num;
+    vector<int> num, segTree;
     NumArray(vector<int>& nums) {
         num=nums;
-    }
-    
-    int sumRange(int left, int right) {
-        int sum=0;
-        for(int i=left;i<=right;i++){
-            sum+=num[i];
+        int n=nums.size();
+        segTree.resize(4*n);
+        buildTree(0,0,n-1,segTree,nums);
+        for(auto it: segTree){
+            cout<<it<<' ';
         }
-        return sum;
+    }
+
+    void buildTree(int i, int l, int r, vector<int>& segTree, vector<int>& nums){
+        if(l==r){
+            segTree[i]=nums[r];
+            return;
+        }
+        int mid=(l+r)/2;
+        buildTree(2*i+1,l,mid,segTree,nums);
+        buildTree(2*i+2,mid+1,r,segTree,nums);
+        segTree[i]=segTree[2*i+1]+segTree[2*i+2];
+    }
+
+    int query(int i, int l, int r, int st, int end, vector<int>& segTree){
+        if(l>end || r<st) return 0;
+        if(l>=st && r<=end) return segTree[i];
+        int mid=(l+r)/2;
+        int left = query(2*i+1, l, mid, st, end, segTree);
+        int right = query(2*i+2, mid+1, r, st, end, segTree);
+        return left+right;
+    }
+
+    int sumRange(int left, int right) {
+        int n=num.size();
+        return query(0, 0, n-1, left, right, segTree);
     }
 };
 
