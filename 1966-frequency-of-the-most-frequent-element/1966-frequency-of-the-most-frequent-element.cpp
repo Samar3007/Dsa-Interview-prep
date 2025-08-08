0fb84cfd07e734fd49b27863f216158f6f8d2abe
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2025-08-08 18:36:00 +0530

    Time: 50 ms (18.38%), Space: 106 MB (29.41%) - LeetHub

--- 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
-    int binary(int l, int r, int k, vector<long>& pref, int ele, int ind){
-        int curr=0;
-        while(l<=r){
-            int mid=(l+r)/2;
-            long sum=long(ind-mid+1)*long(ele);
-            long prefSum=(pref[ind+1]-pref[mid]);
-            long diff=sum-prefSum;
-            if(diff<=k){
-                r=mid-1;
-                curr=max(curr,ind-mid+1);
-            }
-            else{
-                l=mid+1;
-            }
-        }
-        return curr;
-    }
-
     int maxFrequency(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
         int maxi=1, n=nums.size();
-        vector<long> pref(n+1,0);
-        for(int i=0;i<n;i++){
-            pref[i+1]=pref[i]+nums[i];
-        }
+        int i=0, j=0;
+        long currSum=0;
+        
+        while(j<n){
+            currSum+=nums[j];
+            int target=nums[j];
+            long targetSum=long(j-i+1)*target;
+            long diff=targetSum-currSum;
 
-        for(int i=0;i<n;i++){
-            int curr=0;
-            curr=binary(0,i,k,pref,nums[i],i);
-            maxi=max(curr,maxi);
+            while(diff>k){
+                currSum-=nums[i];
+                i++;
+                targetSum=long(j-i+1)*target;
+                diff=targetSum-currSum;
+            }
+            maxi=max(maxi,j-i+1);
+            j++;
         }
+        
         return maxi;
     }
 };
