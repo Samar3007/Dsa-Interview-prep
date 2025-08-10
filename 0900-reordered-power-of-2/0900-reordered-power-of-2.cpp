c9ff5fcd8846b90206e9f4d4a9e60a652fd70155
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2025-08-10 16:33:33 +0530

    Time: 0 ms (100%), Space: 9.3 MB (34.22%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
@@ -5,36 +5,48 @@ public:
         return num%n==0;
     }
     
-    bool solve(int i, vector<int> &freq, int cnt, int num, bool first){
-        if(cnt==i){
-            cout<<num<<' ';
-            return check(num);
-        }
+    // bool solve(int i, vector<int> &freq, int cnt, int num, bool first){
+    //     if(cnt==i){
+    //         cout<<num<<' ';
+    //         return check(num);
+    //     }
     
-        bool take=0;
-        for(int j=0;j<10;j++){
-            if(freq[j]>0){
-                if(j==0 && first) continue;
-                else{
-                    freq[j]--;
-                    if(solve(i+1,freq,cnt,num*10+j,false)) return true;
-                    freq[j]++;
-                }
-            }
-        }
-        return false;
+    //     bool take=0;
+    //     for(int j=0;j<10;j++){
+    //         if(freq[j]>0){
+    //             if(j==0 && first) continue;
+    //             else{
+    //                 freq[j]--;
+    //                 if(solve(i+1,freq,cnt,num*10+j,false)) return true;
+    //                 freq[j]++;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    string sortedStr(int n){
+        string s=to_string(n);
+        sort(s.begin(), s.end());
+        return s;
     }
 
     bool reorderedPowerOf2(int n) {
         if(n==0) return false;
-        vector<int> freq(10);
-        int num=n, cnt=0;
-        while(num){
-            freq[num%10]++;
-            num/=10;
-            cnt++;
-        }
+        // vector<int> freq(10);
+        // int num=n, cnt=0;
+        // while(num){
+        //     freq[num%10]++;
+        //     num/=10;
+        //     cnt++;
+        // }
+        // return solve(0,freq,cnt,0,true);
 
-        return solve(0,freq,cnt,0,true);
+        string s=sortedStr(n);
+
+        for(int i=0;i<30;i++){
+            if(s == sortedStr(1<<i)) return true;
+        }
+        return false;
     }
 };
