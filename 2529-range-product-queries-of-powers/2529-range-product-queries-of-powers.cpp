be34ea3add6d1c1e34f320cb29dfcf826978e4a3
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2025-08-11 17:05:21 +0530

    Time: 20 ms (61.7%), Space: 147.5 MB (69.68%) - LeetHub

---
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int mod=1e9+7;
+    vector<int> genArr(int i, int n, int sum){
+        vector<int> temp;
+        
+        while(sum != n){
+            int num=1<<i;
+            if(num <= n-sum){
+                sum+=num;
+                temp.push_back(num);
+            }
+            i--;
+        }
+        return temp;
+    }
+
+    vector<int> givePowers(int n, int i){
+        while(1<<i <= n){
+            i++;
+        }
+        vector<int> res = genArr(i-1,n,0);
+        
+        sort(res.begin(),res.end());
+        return res;
+    }
+
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        vector<int> powers=givePowers(n,0);
+        int len=powers.size();
+        
+        // vector<long> prefProd(len+1,1);
+        // for(int i=0;i<len;i++){
+        //     prefProd[i+1]=(prefProd[i]*powers[i]) % mod;
+        //     cout<<powers[i]<<' ';
+
+        // }
+
+        vector<int> res;
+        for(auto &query : queries){
+            int st=query[0], end=query[1];
+            long ans=1;
+            for(int i=st;i<=end;i++){
+                ans=(ans*powers[i])%mod;
+            }
+            res.push_back(ans);
+        }
+
+        return res;
+    }
+};
