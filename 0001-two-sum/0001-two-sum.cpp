80605f185f09da456ce68da45cf85acf8c127805
Samar3007 <74319774+Samar3007@users.noreply.github.com>
2026-04-08 18:50:53 +0530

    Time: 0 ms (100%), Space: 14.8 MB (53.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
-
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target) return {i,j};
+         // map stores: value -> index
+        unordered_map<int, int> numMap; 
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            int currentNum = nums[i];
+            int complement = target - currentNum;
+            
+            // Check if the complement exists in our map
+            if (numMap.count(complement)) {
+                // If found, we have our pair!
+                // The complement's index is stored in the map, current num's index is 'i'
+                return {numMap[complement], i};
             }
+            
+            // If complement not found, add the current number and its index to the map
+            numMap[currentNum] = i;
         }
-        return {-1,-1};
+        
+        // This line should technically not be reached given the problem constraints
+        // (exactly one solution exists). But it's good practice for functions that
+        // promise a return value.
+        return {}; 
     }
 };
